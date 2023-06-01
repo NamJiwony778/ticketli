d260cf7b13385369632c42d1031e9f4a20daf537
Mariia <mariia@guarana.io>
2023-05-31 21:27:48 -0400

    api features added

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -3,7 +3,11 @@ const catchAsync = require("../utils/catchAsync");
 const APIFeatures = require("../utils/apiFeatures");
 
 exports.getAllEvents = catchAsync(async (req, res, next) => {
-  const features = new APIFeatures(Event.find(), req.query).filter();
+  const features = new APIFeatures(Event.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
   const events = await features.query;
   res.status(200).json({
     status: "success",
--- server/utils/apiFeatures.js
+++ server/utils/apiFeatures.js
@@ -10,13 +10,42 @@ class APIFeatures {
     const excludedFields = ["sort", "page", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
     let queryStr = JSON.stringify(queryObj);
-    this.queryString = queryStr.replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      (match) => `$${match}`
-    );
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
     this.query = this.query.find(JSON.parse(queryStr));
 
     return this;
   }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("year");
+    }
+    return this;
+  }
+
+  //show chosen fields
+  //127.0.0.1:3000/api/v1/books?fields=year
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select("-__v");
+    }
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
 }
+
+module.exports = APIFeatures;
